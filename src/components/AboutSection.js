3d0ac3acd294bb5b2848ca96208440a4e7f9ed3e
Petr Simak <petr.simak@honeywell.com>
2020-12-09 13:41:56 +0100

    Page Animations added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-// Styled
-import styled from 'styled-components';
-import {About, Description, Image, Hide} from "../styles";
 
+import {About, Description, Image, Hide} from "../styles";
+// Framer Motion
+import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnim} from "../animation";
 
 const AboutSection = () => {
     return (
         <About>
             <Description>
-                <div className='title'>
+                <motion.div >
                     <Hide>
-                        <h2>We work to make </h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come </h2>
+                        <motion.h2 variants={titleAnim}> your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true. </h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
-                </div>
-                <p>Contact us for any photography or videography ideas that you have. We have proffesionals with amazing
-                    skills.</p>
-                <button>Contact Us</button>
-                </Description>
-                <Image>
-                    <img src={home1} alt='guy with a camera'/>
-                </Image>
+                </motion.div>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have proffesionals with amazing
+                    skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
+            </Description>
+            <Image>
+                <motion.img variants={photoAnim} src={home1} alt='guy with a camera'/>
+            </Image>
         </About>
     );
 };
