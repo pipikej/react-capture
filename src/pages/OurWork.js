4a6beae09d92f6bba3018907805885de033e5102
Petr Simak <petr.simak@honeywell.com>
2020-12-09 17:50:26 +0100

    Mobile Friendly styling added.

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -56,6 +56,9 @@ const Work = styled(motion.div)`
   overflow: hidden;
   padding: 5rem 10rem;
   background: white;
+  @media(max-width: 1300px){
+    padding: 2rem 2rem;
+  }
   h2{
     padding: 1rem 0rem;
   }
